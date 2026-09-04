db87f2a2252391e6470bdfa8234cd1b09695a30a
SKANDER <macbook@device-487.home>
2026-09-04 21:36:04 +0200

    fix: allow external Pokemon image domains in next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Force SSR - no static export
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.pokemon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.pokemondb.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'archives.bulbagarden.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.nintendo.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
